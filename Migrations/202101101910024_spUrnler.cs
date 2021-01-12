e985b3c7def4b1cef13cff5195e609dbeea114a2
Volkan Altıntaş <77174268+Volkan-Altintas@users.noreply.github.com>
2021-01-13 02:39:38 +0300

    Migrations kayıtları

---
+++ Migrations/202101101910024_spUrnler.cs
@@ -0,0 +1,29 @@
+﻿namespace finalProje.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class spUrnler : DbMigration
+    {
+        public override void Up()
+        {
+            CreateStoredProcedure
+           (
+               "dbo.cokSatanlar",
+               t => new
+               {
+               },
+               body:
+               @"
+                select sum(urunAdet),urunAdı
+                from SatisListesis
+                group by urunAdı
+                "
+            );
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
